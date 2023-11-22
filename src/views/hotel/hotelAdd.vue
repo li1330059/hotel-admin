96afe83a704c96e297a1bbb3b7366d814f3abf6b
li1330059 <956776191@qq.com>
2023-11-22 21:22:08 +0800

    banner

--- src/views/hotel/hotelAdd.vue
+++ src/views/hotel/hotelAdd.vue
@@ -19,6 +19,25 @@
       <el-form-item label="酒店详情">
         <el-input v-model="form.value" />
       </el-form-item>
+      <el-form-item label="酒店轮播图片">
+        <div class="list">
+          <div v-for="(item,index) in form.bannertp" :key="index" class="list-item">
+            <el-upload
+              class="avatar-uploader"
+              action="http://admin.zjjatjd.top/admin/upload"
+              accept=".jpg,.jpeg,.png,.svg,.svga,gif,JPG,JPEG,PNG,GIF"
+              :show-file-list="false"
+              :on-success="(response, file, fileList) => handleBannerSuccess(response, file, fileList, index)">
+              <img v-if="item.img" :src="item.img" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+            <div class="ctrl">
+              <el-button type="danger" @click="del(index)">删除</el-button>
+            </div>
+          </div>
+        </div>
+        <el-button type="primary" @click="addPic">添加图片</el-button>
+      </el-form-item>
       <el-form-item label="酒店图片">
         <el-upload
           class="avatar-uploader"
@@ -59,7 +78,8 @@ export default {
         dz: '',
         value: '',
         tp:'',
-        status:0
+        status:0,
+        bannertp:[]
       }
     }
   },
@@ -75,6 +95,19 @@ export default {
   methods: {
     handleAvatarSuccess(res, file) {
       this.form.tp = res.data.file_name;
+    },
+    handleBannerSuccess(response, file, fileList, index) {
+      this.form.bannertp[index].img = response.data.file_name;
+    },
+    del(index) {
+      this.form.bannertp.splice(index, 1);
+    },
+    addPic(){
+      this.form.bannertp.push(
+        {
+          img:""
+        }
+      )
     },
     onSubmit() {
       if(!this.$route.query.id){
@@ -142,6 +175,20 @@ export default {
   width: 320px;
   height: 180px;
   display: block;
+}
+.list{
+  display: flex;
+  flex-wrap: wrap;
+  .list-item{
+    .ctrl{
+      width: 100%;
+      display: flex;
+    }
+    margin: 0 20px 20px 0;
+  }
+  .avatar-uploader{
+    margin-bottom: 10px;
+  }
 }
 </style>
 
